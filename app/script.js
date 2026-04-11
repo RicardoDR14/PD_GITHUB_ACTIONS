fa9b2e655052ff4ae441f9f7b6000fba57227d70
Ruben Ferreiro <47047721+Dragneel277@users.noreply.github.com>
2026-04-11 12:36:37 +0100

    Add files via upload

--- app/script.js
+++ app/script.js
@@ -1,12 +1,17 @@
 const themeToggle = document.getElementById("themeToggle");
 const themeIcon = document.getElementById("themeIcon");
 
-themeToggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-
+function updateThemeIcon() {
   if (document.body.classList.contains("dark-mode")) {
     themeIcon.textContent = "☾";
   } else {
     themeIcon.textContent = "☀";
   }
+}
+
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  updateThemeIcon();
 });
+
+updateThemeIcon();
